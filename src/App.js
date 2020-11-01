ed9d1ef22e8afa9a6593b055f0d58cbadf2b8510
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-11-01 14:41:14 +0530

    Ayush | Working sample navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+import Team from './pages/Team';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ayush</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/reports' component={Reports}/>
+          <Route path='/products' component={Products}/>      
+          <Route path='/team' component={Team}/>                    
+        </Switch>
+      </Router>
+    </>
   );
 }
 
